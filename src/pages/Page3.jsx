f67eb1541f594c0279c7af113c38b81310ff777c
jinyikim <jinyikim994@gmail.com>
2025-11-20 16:57:23 +0900

    251120 최종

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -5,6 +5,8 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 // 이미지, 비디오 불러오기
 import MainSlatePoster from "../assets/images/main_slate.jpg";
+import MainIcon1 from "../assets/images/main_plai.png";
+import MainIcon2 from "../assets/images/main_oneteam.png";
 import MainSlateVideo from "../assets/videos/main_slate.mp4";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -17,15 +19,15 @@ const MainPage = () => {
         const slateSection = slateRef.current;
 
         const slateTimeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: slateSection,
-            start: "top top",
-            end: "+=600%",
-            scrub: 1,
-            pin: true,
-            anticipatePin: 1
-        }
-        });
+            scrollTrigger: {
+                trigger: slateSection,
+                start: "top top",
+                end: "+=600%",
+                scrub: 1,
+                pin: true,
+                anticipatePin: 1
+            }
+            });
 
         const upSpan = slateSection.querySelector(".slate-up span");
         const downSpan = slateSection.querySelector(".slate-down span");
@@ -34,63 +36,64 @@ const MainPage = () => {
         const vod = slateSection.querySelector(".slate-vod");
 
         slateTimeline
-        .to(upSpan, { y: "-50px", ease: "power1.in", duration: 0.1 }, 0)
-        .to(downSpan, { y: "50px", ease: "power1.in", duration: 0.1 }, 0)
-        .to(upBox, { y: "-100%", ease: "power1.in", duration: 0.1 }, 0.1)
-        .to(downBox, { y: "100%", ease: "power1.in", duration: 0.1 }, 0.1)
-        .to(
-            vod,
-            {
-            clipPath:
-                "inset(calc(50% - 300px) calc(50% - 640px) round 30px)",
-            duration: 0.1
-            }
-        );
+            .to(upSpan, { y: "-50px", ease: "power1.in", duration: 0.1 }, 0)
+            .to(downSpan, { y: "50px", ease: "power1.in", duration: 0.1 }, 0)
+            .to(upBox, { y: "-100%", ease: "power1.in", duration: 0.1 }, 0.1)
+            .to(downBox, { y: "100%", ease: "power1.in", duration: 0.1 }, 0.1)
+            .to(
+                vod,
+                {
+                clipPath:
+                    "inset(calc(50% - 300px) calc(50% - 640px) round 30px)",
+                duration: 0.1
+                }
+            );
 
         return () => {
-        if (slateTimeline.scrollTrigger) slateTimeline.scrollTrigger.kill();
-        slateTimeline.kill();
-        };
-    }, []);
+            if (slateTimeline.scrollTrigger) slateTimeline.scrollTrigger.kill();
+            slateTimeline.kill();
+            };
+        }, []);
 
     /* ---------------------- 두 번째 섹션(MainZoom) ---------------------- */
     const zoomRef = useRef(null);
 
-    useEffect(() => {
-        const zoomTimeline = gsap.timeline({
-        scrollTrigger: {
-            trigger: zoomRef.current,
-            start: "top top",
-            end: "+=150%",
-            scrub: 1,
-            pin: true,
-            anticipatePin: 1
-        }
-        });
+        useEffect(() => {
+            const zoomTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: zoomRef.current,
+                start: "top top",
+                end: "+=150%",
+                scrub: 1,
+                pin: true,
+                anticipatePin: 1
+            }
+            });
 
-        zoomTimeline
-        .to(".zoom-group", { clipPath: "inset(-10% round 20px)" })
-        .to(".zoom-txt > strong", { color: "#2D3EBD" }, 0.5)
-        .fromTo(
-            ".zoom-obj",
-            { width: "0", margin: "0" },
-            {
-            width: "auto",
-            margin: "0 5px 0 10px",
-            duration: 0.2
-            },
-            0.5
-        )
-        .to(".main-zoom", { backgroundColor: "transparent" });
+            zoomTimeline
+            .to(".zoom-group", { clipPath: "inset(-10% round 20px)" })
+            .to(".zoom-txt > strong", { color: "#2D3EBD" }, 0.5)
+            .fromTo(
+                ".zoom-obj",
+                { width: "0", margin: "0" },
+                {
+                width: "auto",
+                margin: "0 5px 0 10px",
+                duration: 0.2
+                },
+                0.5
+            )
+            .to(".main-zoom", { backgroundColor: "transparent" });
 
-        return () => {
-        if (zoomTimeline.scrollTrigger) zoomTimeline.scrollTrigger.kill();
-        zoomTimeline.kill();
-        };
-    }, []);
+            return () => {
+            if (zoomTimeline.scrollTrigger) zoomTimeline.scrollTrigger.kill();
+            zoomTimeline.kill();
+            };
+        }, []);
 
     /* ---------------------- 리턴 (두 섹션 통합) ---------------------- */
     return (
+        
         <div className="slate-wrapper">
             {/* ---------- MainSlate 섹션 ---------- */}
             <section
@@ -131,35 +134,15 @@ const MainPage = () => {
             >
                 <div className="zoom-group flex justify-center items-center bg-white">
                     <div className="zoom-txt font-800 text-44 leading-89">
-                        그 동안 고객과 셀러의
-                        <br className="ver-mo" />
-                        연결성을 강화하기 위해
-                        <br />
-                        독립적이고 자율적인
-                        <br className="ver-mo" />
-                        연합군처럼 움직여 왔지만,
-                        <br />
+                        그 동안 고객과 셀러의  연결성을 강화하기 위해<br />
+                        독립적이고 자율적인 연합군처럼 움직여 왔지만,<br />
                         <strong>생성형 Ai 플레이</strong>
-                        <img
-                        src="./images/main_plai.png"
-                        className="zoom-obj align-middle"
-                        alt=""
-                        />
-                        를
-                        <br className="ver-mo" />
-                        기반으로 한
-                        <br className="ver-mo" />
-                        <strong>One Team</strong>
-                        <img
-                        src="./images/main_oneteam.png"
-                        className="zoom-obj align-middle"
-                        alt=""
-                        />
-                        으로서
-                        <br />
-                        시너지 극대화에
-                        <br className="ver-mo" />
-                        최선을 다하고 있습니다.
+                        {/* 이미지 */}
+                        <img src={MainIcon1} alt="" className="zoom-obj" />
+                        를 기반으로 한 <strong>One Team</strong>
+                        <img src={MainIcon2} alt="" className="zoom-obj"/>
+                        으로서<br />
+                        시너지 극대화에 최선을 다하고 있습니다.
                     </div>
                 </div>
             </section>
